18d7f0f240e6e81a761607253248e044a16112d6
manbow1206 <tyasunaga@iyell.jp>
2022-03-20 12:11:50 +0900

    add : beforeEach

--- tests/unit/click.spec.js
+++ tests/unit/click.spec.js
@@ -2,8 +2,17 @@ import { mount } from '@vue/test-utils';
 import ClickVue from '../../src/components/Click.vue';
 
 describe('Click Component Test', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(ClickVue);
+  });
+
   it('should ', async () => {
-    const wrapper = mount(ClickVue);
+    await wrapper.get('button').trigger('click');
+    expect(wrapper.text()).toContain('Count is: 1');
+  });
+
+  it('should ', async () => {
     await wrapper.get('button').trigger('click');
     expect(wrapper.text()).toContain('Count is: 1');
   });
