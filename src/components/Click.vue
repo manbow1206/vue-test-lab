04fd0e6ee387dc2d1dfdee8c92b5b4633c20db5c
manbow1206 <tyasunaga@iyell.jp>
2022-03-20 11:54:08 +0900

    add : Click Event Test

---
+++ src/components/Click.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from '@vue/reactivity';
+
+const count = ref(0);
+
+const increment = () => {
+  count.value += 1;
+};
+</script>
+
+<template>
+  <p>Count is: {{ count }}</p>
+  <button @click="increment">+</button>
+</template>
